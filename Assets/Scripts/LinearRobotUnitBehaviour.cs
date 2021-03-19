71c0b6af3785f47d680435c348b0634a4667cae8
DylanPerdigao <32035460+DylanPerdigao@users.noreply.github.com>
2021-03-19 18:44:03 +0000

    possibilidade de escolher os limites no unity

--- Assets/Scripts/LinearRobotUnitBehaviour.cs
+++ Assets/Scripts/LinearRobotUnitBehaviour.cs
@@ -7,9 +7,12 @@
 public class LinearRobotUnitBehaviour : RobotUnit
 {
     public enum Function{Linear,Logarithmic,Gaussian};
+    public enum Limits{ AllLimits, XLimits,YLimits , NoLimits };
 
     public Function wallFunction;
+    public Limits wallLimits;
     public Function resourceFunction;
+    public Limits resourceLimits;
 
     public float weightResource=1;
     public float resourceValue;
@@ -26,29 +29,136 @@ public class LinearRobotUnitBehaviour : RobotUnit
     {
         float resouceAngle = resourcesDetector.GetAngleToClosestResource();
         float wallAngle = blockDetector.GetAngleToClosestObstacle();
-
+        //
+        // ESCOLHE A FUNCAO DE ATIVACAO PARA OS RECURSOS
+        //
         switch (resourceFunction)
         {
             case Function.Linear:
-                resourceValue = weightResource * resourcesDetector.GetLinearOuput();
+                //
+                // ESCOLHE OS LIMITES PARA A FUNCAO LINEAR (PARA OS RECURSOS)
+                //
+                switch (resourceLimits)
+                {
+                    case Limits.NoLimits:
+                        resourceValue = weightResource * resourcesDetector.NonLimitsLinear();
+                        break;
+                    case Limits.XLimits:
+                        resourceValue = weightResource * resourcesDetector.XLimitLinear();
+                        break;
+                    case Limits.YLimits:
+                        resourceValue = weightResource * resourcesDetector.YLimitLinear();
+                        break;
+                    case Limits.AllLimits:
+                        resourceValue = weightResource * resourcesDetector.GetLinearOuput();
+                        break;
+                }
                 break;
             case Function.Logarithmic:
-                resourceValue = weightResource * resourcesDetector.GetLogaritmicOutput();
+                //
+                // ESCOLHE OS LIMITES PARA A FUNCAO LOGARITMICA (PARA OS RECURSOS)
+                //
+                switch (resourceLimits)
+                {
+                    case Limits.NoLimits:
+                        resourceValue = weightResource * resourcesDetector.NonLimitsLog();
+                        break;
+                    case Limits.XLimits:
+                        resourceValue = weightResource * resourcesDetector.XLimitLog();
+                        break;
+                    case Limits.YLimits:
+                        resourceValue = weightResource * resourcesDetector.YLimitLog();
+                        break;
+                    case Limits.AllLimits:
+                        resourceValue = weightResource * resourcesDetector.GetLogaritmicOutput();
+                        break;
+                }
                 break;
             case Function.Gaussian:
-                resourceValue = weightResource * resourcesDetector.GetGaussianOutput();
+                //
+                // ESCOLHE OS LIMITES PARA A FUNCAO GAUSSIANA (PARA OS RECURSOS)
+                //
+                switch (resourceLimits)
+                {
+                    case Limits.NoLimits:
+                        resourceValue = weightResource * resourcesDetector.NonLimitsGauss();
+                        break;
+                    case Limits.XLimits:
+                        resourceValue = weightResource * resourcesDetector.XLimitGauss();
+                        break;
+                    case Limits.YLimits:
+                        resourceValue = weightResource * resourcesDetector.YLimitGauss();
+                        break;
+                    case Limits.AllLimits:
+                        resourceValue = weightResource * resourcesDetector.GetGaussianOutput();
+                        break;
+                }
                 break;
         }
+        //
+        // ESCOLHE A FUNCAO DE ATIVACAO PARA OS OBSTACULOS
+        //
         switch (wallFunction)
         {
             case Function.Linear:
-                wallValue = weightWall * blockDetector.GetLinearOuput();
+                //
+                // ESCOLHE OS LIMITES PARA A FUNCAO LINEAR (PARA OS OBSTACULOS)
+                //
+                switch (wallLimits)
+                {
+                    case Limits.NoLimits:
+                        wallValue = weightWall * blockDetector.NonLimitsLinear();
+                        break;
+                    case Limits.XLimits:
+                        wallValue = weightWall * blockDetector.XLimitLinear();
+                        break;
+                    case Limits.YLimits:
+                        wallValue = weightWall * blockDetector.YLimitLinear();
+                        break;
+                    case Limits.AllLimits:
+                        wallValue = weightWall * blockDetector.GetLinearOuput();
+                        break;
+                }
                 break;
             case Function.Logarithmic:
-                wallValue = weightWall * blockDetector.GetLogaritmicOutput();
+                //
+                // ESCOLHE OS LIMITES PARA A FUNCAO LOGARITMICA (PARA OS OBSTACULOS)
+                //
+                switch (wallLimits)
+                {
+                    case Limits.NoLimits:
+                        wallValue = weightWall * blockDetector.NonLimitsLog();
+                        break;
+                    case Limits.XLimits:
+                        wallValue = weightWall * blockDetector.XLimitLog();
+                        break;
+                    case Limits.YLimits:
+                        wallValue = weightWall * blockDetector.YLimitLog();
+                        break;
+                    case Limits.AllLimits:
+                        wallValue = weightWall * blockDetector.GetLogaritmicOutput();
+                        break;
+                }
                 break;
             case Function.Gaussian:
-                wallValue = weightWall * blockDetector.GetGaussianOutput();
+                //
+                // ESCOLHE OS LIMITES PARA A FUNCAO GAUSSIANA (PARA OS OBSTACULOS)
+                //
+                switch (wallLimits)
+                {
+                    case Limits.NoLimits:
+                        wallValue = weightWall * blockDetector.NonLimitsGauss();
+                        break;
+                    case Limits.XLimits:
+                        wallValue = weightWall * blockDetector.XLimitGauss();
+                        break;
+                    case Limits.YLimits:
+                        wallValue = weightWall * blockDetector.YLimitGauss();
+                        break;
+                    case Limits.AllLimits:
+                        wallValue = weightWall * blockDetector.GetGaussianOutput();
+                        break;
+                }
                 break;
         }
 
